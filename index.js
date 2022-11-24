6f01661bd8ff19ec6421cf737bb4ca2b2c4aa4ce
perragnarl <perragnar.lindfors@gmail.com>
2022-11-24 19:27:33 +0100

    Recursive mkdir

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function main() {
     type: "input",
     name: "file",
     message: "What file would you like to read?",
-    default: "./urls.json",
+    default: "urls.json",
   });
 
   return file.file;
@@ -77,8 +77,8 @@ function navigateTo(url, page) {
 
 function takeScreenshot(page, pageName, fileName) {
   return new Promise((resolve, reject) => {
-    if (!fs.existsSync(`screenshots/${pageName}`)) {
-      fs.mkdirSync(`screenshots/${pageName}`);
+    if (!fs.existsSync(`screenshots/${pageName}`, { recursive: true })) {
+      fs.mkdirSync(`screenshots/${pageName}`, { recursive: true });
     }
 
     page
